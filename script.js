ea7434f72eb9fc268a3bc0dc9558495dd3f1fc8f
Cuba Gustafsson <trill007@hotmail.com>
2022-06-07 10:33:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+} else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode= this.dataset.mode
+        console.log('Option clicked: ', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode)
+{
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'style.css'
+    }
+     if (mode == "sunset") {
+       document.getElementById("theme-style").href = 'sunset.css';
+     }
+      if (mode == "blue") {
+        document.getElementById("theme-style").href = 'blue.css';
+      }
+       if (mode == "fantasy") {
+         document.getElementById("theme-style").href = 'fantasy.css';
+       }
+        if (mode == "dark") {
+          document.getElementById("theme-style").href = 'dark.css';
+        }
+        localStorage.setItem('theme', mode)
+
+}
